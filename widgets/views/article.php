6298fa3ed478cfe6abe637cc61d6f26969b46c95
dmitry <kyrnickijdima@gmail.com>
2019-10-27 16:53:10 +0200

    фикс по верстке

--- widgets/views/article.php
+++ widgets/views/article.php
@@ -11,7 +11,7 @@ use yii\helpers\StringHelper;
 
 <li class="span4 gallery-item">
     <a class="my-index-link" href="<?= Url::to(['/articles/view', 'slug' => $article->slug]); ?>" title="Читати статтю">
-        <img class="my-index-image" src="<?= $article->getImageSrc() ?>" alt="picture">
+        <img class="my-article-image" src="<?= $article->getImageSrc() ?>" alt="picture">
     </a>
     <span class="project-details">
         <a href="<?= Url::to(['/articles/view', 'slug' => $article->slug]); ?>">
@@ -20,14 +20,3 @@ use yii\helpers\StringHelper;
         <?= StringHelper::truncate($article->subtitle,115); ?>
     </span>
 </li>
-
-<style>
-    img {
-        max-height: 209px;
-    }
-    @media (max-width: 767px) {
-        img {
-            max-height: none;
-        }
-    }
-</style>
